d486154b76a0bc03d95637bb4f760157553d2976
Tedydev <Hieudat2310.bh@gmail.com>
2025-06-25 23:18:43 +0700

    Cập nhật dịch vụ và bộ điều khiển để thêm chức năng lấy cookie: thay đổi phương thức getHello thành getCookies trong AppService và bổ sung endpoint /get-cookies trong AppController với decorator IsPublic.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,14 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { IsPublic } from './shared/decorators/auth.decorator'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @IsPublic()
+  @Get('/get-cookies')
+  getCookies(): string {
+    return this.appService.getCookies()
+  }
 }
